c819dc939bd984dd468b788a8ad67104c7eaa69c
SimeonGeorgiev <simeon.georgiev@estafet.com>
2022-09-01 15:25:27 +0300

    fixes

--- src/main/java/Databases/DatabaseFactory/Database.java
+++
@@ -1,5 +0,0 @@
-package Databases.DatabaseFactory;
-
-public interface Database {
-    void connect();
-}
--- src/main/java/Databases/DatabaseFactory/DatabaseTest.java
+++
@@ -1,9 +0,0 @@
-package Databases.DatabaseFactory;
-
-public class DatabaseTest {
-    public static void main(String[] args) {
-        Database database = DatabaseFactory.connect("localhost");
-        Database database2 = DatabaseFactory.connect("localhost2");
-    }
-}
-
